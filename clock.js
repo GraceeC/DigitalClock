bff1e19342e0afb7c79de3b56bfef49e7065bda1
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-11-10 18:58:29 -0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,6 @@
 
+//using Javascript build in Object Date for var value
+const fullDate = new Date();
+const hours = fullDate.getHours();
+const minutes = fullDate.getMinutes();
+console.log(fullDate);
