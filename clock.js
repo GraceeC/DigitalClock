3fa888b293b8ff79f0818c228cc4b115f4f39232
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-11-10 19:22:56 -0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,6 +1,14 @@
-
+//diGital Functio will hold all storage
+function digitalClock(){
 //using Javascript build in Object Date for var value
 const fullDate = new Date();
 const hours = fullDate.getHours();
 const minutes = fullDate.getMinutes();
+const seconds = fullDate.getSeconds();
 console.log(fullDate);
+
+//grabbing the id of the span to give var value from document
+document.getElementById('hours').innerHTML = hours;
+document.getElementById('minutes').innerHTML = minutes;
+document.getElementById('seconds').innerHTML = seconds;
+}
