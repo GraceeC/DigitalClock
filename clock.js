0909320e5d9fd805212e37ff0639bf8e9b7ffaf0
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-11-13 19:10:01 -0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,14 +1,36 @@
 //diGital Functio will hold all storage
-function digitalClock(){
-//using Javascript build in Object Date for var value
-const fullDate = new Date();
-const hours = fullDate.getHours();
-const minutes = fullDate.getMinutes();
-const seconds = fullDate.getSeconds();
-console.log(fullDate);
+function digitalClock() {
+  //using Javascript build in Object Date for var value
+  let fullDate = new Date();
+  let hours = fullDate.getHours();
+  let minutes = fullDate.getMinutes();
+  let seconds = fullDate.getSeconds();
+  
 
-//grabbing the id of the span to give var value from document
-document.getElementById('hours').innerHTML = hours;
-document.getElementById('minutes').innerHTML = minutes;
-document.getElementById('seconds').innerHTML = seconds;
+  //grabbing the id of the span to give variable value from html document
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  digitalClock();
+
+//month array
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  document.getElementById("clock").innerHTML = months[fullDate.getMonth()];
+  //getting todays date
 }
+//called function to stop time from delaying on page being refreshed
+
+setInterval(digitalClock, 1000);
