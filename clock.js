4d290e7e0b60c7844705a1fcea863c514082691c
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-11-18 20:55:01 -0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,22 +2,38 @@
 function digitalClock() {
   //using Javascript build in Object Date for var value
   let fullDate = new Date();
-  let hours = fullDate.getHours();
-  let minutes = fullDate.getMinutes();
-  let seconds = fullDate.getSeconds();
-   
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
+  let hour = fullDate.getHours();
+  let minute = fullDate.getMinutes();
+  let second = fullDate.getSeconds();
+  let btn= document.getElementById("button");
+  // let time="am";
+  // if (hour>=12){
+  //   time="pm";
+  //   hour=hour -12;
+  // }
+  // if (hour ==0){
+  //   hour =12;
+  // }
   
-  //variables for day 
-  let months = ["January","February","March", "April","May","June", "July","August","September","October","November", "December"
-  ];
-  let today = fullDate.getDate();
-//getting date 
-  document.getElementById("months").innerHTML = months[fullDate.getMonth()];
-  console.log(today);
-}
-//called function to stop time from delaying on page being refreshed
-digitalClock() 
-setInterval(digitalClock, 1000);
+//displaying todays date/ //adding 1 due to months going 0-11 in Javascript
+  let date = fullDate.getMonth() + 1 +"-" +fullDate.getDate() + "-" + fullDate.getFullYear();
+
+  //grabbing the id of the span to give variable value from html document
+  document.getElementById("hours").innerHTML = hour;
+  document.getElementById("minutes").innerHTML = minute;
+  document.getElementById("seconds").innerHTML = second;
+  //gsetting todays date
+  
+  document.getElementById("day").innerHTML = date;
+  btn.addEventListener("click",function(){
+  console.log("clicked")
+  });
+
+
+};
+  //will make time run every second
+  setInterval(digitalClock, 1000);
+    
+
+//called function to stop time from delaying on page
+digitalClock();
